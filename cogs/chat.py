055d819ad6a0c0a1bc9e40329f435610214552e4
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-03-03 16:11:14 -0600

    yep

--- cogs/chat.py
+++ cogs/chat.py
@@ -9,27 +9,25 @@ url = "https://api.openai.com/v1/chat/completions"
 headers = {"Content-Type": "application/json"}
 
 
-class chat(commands.Cog):
+class freaq(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.chat_history = ""
 
-    @commands.command(name='chat')
-    async def chat(self, ctx, *args):
+    @commands.command(name='freaq')
+    async def freaq(self, ctx, *args):
         user_input = ' '.join(args)
         self.chat_history += "\nUser: " + user_input
         print("chat is doing first something")  
         response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=[
-                {"role": "system", "content": "you are a nice bot named freaq.  you can answer any questions.   try to encourage people to be kind"},
+                {"role": "system", "content": "you are a nice bot named freaq. although freak can have bad connotations, freaq is an endearing name for you. you can answer any questions.   try to encourage people to be kind. try use informal language.  try to use internet language. if you disagree with someone, call them cringe.  you can use other internet terminology or emojis."},
               {"role": "user", "content": user_input},
                 {"role": "assistant", "content": self.chat_history}
                 ]
             )
-        print(response)
         print('chat is doing second something')
-        print(response)
         await ctx.send(response['choices'][0]['message']['content'])
         
     @commands.command(name='clear_chat')
@@ -38,5 +36,5 @@ class chat(commands.Cog):
             await ctx.send("history cleared, talk soon love you")
 
 async def setup(bot):
-    await bot.add_cog(chat(bot))
+    await bot.add_cog(freaq(bot))
 
